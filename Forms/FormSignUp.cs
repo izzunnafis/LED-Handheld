69a191a5b581e4fa84763a30e9c840fc26ab9c01
Raihan Dhiazuhdi <raihandhiazuhdi@gmail.com>
2021-02-15 10:42:12 +0700

    Login

--- Forms/FormSignUp.cs
+++ Forms/FormSignUp.cs
@@ -56,7 +56,8 @@ namespace LED_Handheld_Project.Forms
                 MessageBox.Show("Please enter the same password");
             else
             {
-                string mainconn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Mahasiswa UGM\Kerja Praktik\Visual Studio\GitHub LED Handheld Project\LoginDB.mdf;Integrated Security=True;Connect Timeout=30";
+                string mainconn = ConfigurationManager.ConnectionStrings["LED_Handheld_Project.Properties.Settings.cn"].ConnectionString;
+                //string mainconn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Mahasiswa UGM\Kerja Praktik\Visual Studio\GitHub LED Handheld Project\LoginDB.mdf;Integrated Security=True;Connect Timeout=30";
                 SqlConnection sqlconn = new SqlConnection(mainconn);
                 string queary = "Insert into [dbo].[SignUpTable] values (@username, @password, @email)";
                 sqlconn.Open();
