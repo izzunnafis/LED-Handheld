7acb7d881b4ae05c4c512423d45eeb840ec5d75e
imamsyahrizal <imamsyahrizal00@gmail.com>
2021-02-17 19:23:51 +0700

    ui

--- Forms/About.Designer.cs
+++ Forms/About.Designer.cs
@@ -29,10 +29,18 @@ namespace LED_Handheld_Project.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "About";
             this.Text = "About";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
