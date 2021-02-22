fe1b82522ce13c192cfaa8eb8982ffac24df3a02
imamsyahrizal <imamsyahrizal00@gmail.com>
2021-02-22 20:35:35 +0700

    About Form

--- Forms/About.Designer.cs
+++ Forms/About.Designer.cs
@@ -29,8 +29,10 @@ namespace LED_Handheld_Project.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.labelAbout = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,9 +45,9 @@ namespace LED_Handheld_Project.Forms
             this.textBox1.Location = new System.Drawing.Point(51, 108);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1084, 471);
+            this.textBox1.Size = new System.Drawing.Size(1084, 56);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "This software is made for Kerja Praktik PT LEN Industri. This project is de";
+            this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
             // labelAbout
             // 
@@ -60,12 +62,27 @@ namespace LED_Handheld_Project.Forms
             this.labelAbout.TabIndex = 1;
             this.labelAbout.Text = "LED Handheld Project";
             // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox3.Location = new System.Drawing.Point(51, 220);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1084, 132);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = resources.GetString("textBox3.Text");
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
             this.ClientSize = new System.Drawing.Size(1177, 666);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.labelAbout);
             this.Controls.Add(this.textBox1);
             this.Name = "About";
@@ -79,5 +96,6 @@ namespace LED_Handheld_Project.Forms
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label labelAbout;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
